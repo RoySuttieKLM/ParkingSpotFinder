65029583944781b2f2b08e9061b1146a62cd1f99
Roy Suttie <roy.suttie@klm.com>
2022-04-14 10:25:34 +0200

    Changed Name
    Changed the name from Android to Roy

--- app/src/main/java/com/plcoding/mapscomposeguide/MainActivity.kt
+++ app/src/main/java/com/plcoding/mapscomposeguide/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Roy")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MapsComposeGuideTheme {
-        Greeting("Android")
+        Greeting("Roy")
     }
 }
